e17f8c174f48c7754222c08dfb4cb80a6afdc12a
Karate Kid <stauntonsample@gmail.com>
2015-07-18 14:32:25 -0700

    add Vote migration, controller, model, seeds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717031211) do
+ActiveRecord::Schema.define(version: 20150718201611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,14 @@ ActiveRecord::Schema.define(version: 20150717031211) do
     t.date     "dob"
     t.string   "password"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "photo_id"
+    t.boolean  "like"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
