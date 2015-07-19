559fb1c00122dbb41bf7f735a70905806b208cf1
Karate Kid <stauntonsample@gmail.com>
2015-07-18 17:36:09 -0700

    rating flow add

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -16,6 +16,18 @@ class PhotosController < ApplicationController
     vote_count = vote.count
     like_votes = vote.where(:like => true).count
     rating = (like_votes.to_f / vote_count.to_f) * 100
+    if rating >= 90
+      rating_saying = "Smokin!!"
+    elsif rating >= 80
+      rating_saying = "Super Flattering"
+    elsif rating >= 70
+      rating_saying = "So Flattering"
+    elsif rating >= 60
+      rating_saying = "Flattering"
+    elsif rating >= 50
+      rating_saying = "Kind of Flattering"
+    else 
+      rating_saying = "Not Doing You Justice"
     if photo
 
       render :json => {
@@ -23,7 +35,7 @@ class PhotosController < ApplicationController
         :vote_count => vote_count,
         :vote => vote,
         :like_votes => like_votes,
-        :rating => rating
+        :rating_saying => rating_saying
       }
 
     else
