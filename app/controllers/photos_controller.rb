c65e79ed59e936a201c07aef5e531afef42d1904
Karate Kid <stauntonsample@gmail.com>
2015-07-21 15:11:14 -0700

    rename shoe to show_datain photo control

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,7 +12,7 @@ class PhotosController < ApplicationController
     end
   end
 
-  def show
+  def show_data
     photo = Photo.find(params[:id])
     render :json => PhotoRatingCalculator.call(photo)
   end
